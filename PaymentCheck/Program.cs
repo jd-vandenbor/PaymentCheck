b1fea64c3525d794a1978fe57e738123bac6eca0
jd-vandenbor <jd.vandenbor@gmail.com>
2019-08-13 09:27:03 -0400

    Set up rest client and made call to example endpoint

---
+++ PaymentCheck/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PaymentCheckService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            string endpoint = "https://jsonplaceholder.typicode.com/todos/1";
+            RestClient rClient = new RestClient();
+            rClient.endpoint = endpoint;
+            string response = rClient.makeRequest();
+            System.Diagnostics.Debug.WriteLine(response);
+
+
+        }
+
+    }
+}
---
+++ PaymentCheck/RestClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PaymentCheckService
+{
+    public class RestClient
+    {
+
+        public enum httpVerb
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+
+        }
+
+        public string endpoint { get; set; }
+        public httpVerb httpMethod { get; set; }
+
+        public RestClient()
+        {
+            endpoint = string.Empty;
+            httpMethod = httpVerb.GET;
+        }
+
+        public string makeRequest()
+        {
+            string strResponseValue = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endpoint);
+
+            request.Method = httpMethod.ToString();
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new ApplicationException("error code: " + response.StatusCode.ToString());
+                }
+                //Process the response stream... (should be JSON)
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if (responseStream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }//end of using StreamReader
+                    }
+                }//End of using ResponseStream
+
+            }//End of using response
+
+
+            return strResponseValue;
+        }
+
+        //}
+
+    }
+}
